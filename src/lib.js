4942f56e0380fe8142eda166197bfbbec1ff73b1
张培栋 <zhangpeidong@maxtropy.com>
2018-09-14 13:38:03 +0800

    try prettier

--- src/lib.js
+++ src/lib.js
@@ -4,6 +4,5 @@ export const compose = (...fns) => value =>
 export const toNumber = num => parseFloat(num);
 export const isNumber = num => !isNaN(toNumber(num)) && isFinite(toNumber(num));
 export const overLimit = (num, limit) => toNumber(num) === limit;
-
 export const filterNumber = dataSet =>
   Array.isArray(dataSet) ? dataSet.filter(num => isNumber(num)) : [];
